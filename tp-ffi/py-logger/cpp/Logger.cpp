5478044811da3d5a8dd36228e814867810cf5ae2
TODO <TODO>
2024-02-21 17:28:48 +0100

    tp-ffi: py-logger1: WIP Logger (report by level + by added)

--- tp-ffi/py-logger/cpp/Logger.cpp
+++ tp-ffi/py-logger/cpp/Logger.cpp
@@ -4,18 +4,73 @@
 #include <iomanip>
 #include <map>
 #include <sstream>
+#include <vector>
 
-void Logger::addItem(Level l, const std::string & m) {
-  // TODO implement Logger::addItem
+void Logger::addItem(Level l, const std::string &m)
+{
+  _items.push_back(std::make_tuple(l, m));
 }
 
-std::string Logger::reportByAdded() const {
-  // TODO Logger::reportByAdded
-  return "TODO Logger::reportByAdded";
+std::string Logger::reportByAdded() const
+{
+  std::string output = "**** report_by_added ****\n";
+
+  for (const auto &tuple : _items)
+  {
+    switch (std::get<0>(tuple))
+    {
+    case Level::Info:
+      output += "[I]";
+      break;
+    case Level::Warning:
+      output += "[W]";
+      break;
+    case Level::Error:
+      output += "[E]";
+      break;
+    default:
+      output += "[Unknown]";
+    }
+    output += std::get<1>(tuple) + "\n";
+  }
+
+  return output;
 }
 
-std::string Logger::reportByLevel() const {
-  // TODO Logger::reportByLevel
-  return "TODO Logger::reportByLevel";
-}
+std::string Logger::reportByLevel() const
+{
+  std::string output = "**** report_by_level ****\n";
 
+  std::vector<Item> info; 
+  std::vector<Item> warning; 
+  std::vector<Item> error;
+
+  for (auto &tuple: _items)
+  {
+    const auto lev = std::get<0>(tuple); 
+    if (lev == Level::Info){
+      info.push_back(tuple);
+    } else if (lev == Level::Warning) {
+      warning.push_back(tuple);
+    } else if (lev == Level::Error) {
+      error.push_back(tuple);
+    }
+  }
+
+  for (auto &tuple: info)
+  {
+    output += "[I]" + std::get<1>(tuple) + "\n";
+  }
+
+  for (auto &tuple: warning)
+  {
+    output += "[W]" + std::get<1>(tuple) + "\n";
+  }
+
+  for (auto &tuple: error)
+  {
+    output += "[E]" + std::get<1>(tuple) + "\n";
+  }
+
+  return output;
+}
--- tp-ffi/py-logger/scripts/main1.py
+++ tp-ffi/py-logger/scripts/main1.py
@@ -1,11 +1,23 @@
+import Logger
 
 if __name__ == '__main__':
 
     print('TODO')
 
     # TODO create a logger and add some items
+    log = Logger.Logger()
 
+    log.add_item(Logger.Level.Info, "Info 1")
+    log.add_item(Logger.Level.Info, "Info 2")
+    log.add_item(Logger.Level.Info, "Info 3")
+    log.add_item(Logger.Level.Info, "Info 4")
+    log.add_item(Logger.Level.Error, "Error 1")
+    log.add_item(Logger.Level.Error, "Error 2")
+    log.add_item(Logger.Level.Warning, "Warning 1")
     # TODO report_by_added
+    print(log.report_by_added())
 
     # TODO report_by_level
+    print(log.report_by_level())
+
 

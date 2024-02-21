7b56988ffb967d3d4e065bf692d938ba82991aab
metahri.nafaa <metahrinafaapro@gmail.com>
2024-02-21 15:52:05 +0100

    tp-ffi: py-fibo1 : Fibo naive + iterative

--- tp-ffi/py-fibo1/scripts/main.py
+++ tp-ffi/py-fibo1/scripts/main.py
@@ -1,9 +1,9 @@
+import mypackage.myfibo as myfibo
 
 if __name__ == '__main__':
 
-    print('TODO')
+    for i in range (0,10):
+        print(f"fibo_naive({i}) = {myfibo.fibo_naive(i)}")
     
-    # TODO print fibo_naive(i) for i from 0 to 10
-
-    # TODO print fibo_iterative(i) for i from 0 to 10
-
+    for i in range (0,10):
+        print(f"fibo_iterative({i}) = {myfibo.fibo_iterative(i)}")

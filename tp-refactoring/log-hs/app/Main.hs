b0d30867d2e43ec846eb72f70fe7adcc2b8e8ed2
metahri.nafaa <metahrinafaapro@gmail.com>
2024-03-11 14:48:23 +0100

    TP-Refactoring - Injection de dépendances en Haskell

--- tp-refactoring/log-hs/app/Main.hs
+++ tp-refactoring/log-hs/app/Main.hs
@@ -1,3 +1,5 @@
+import System.IO
+
 add3 :: Int -> Int 
 add3 = (+3)
 
@@ -12,9 +14,40 @@ mycompute v0 = do
     let v2 = mul2 v1
     return v2
 
+mycomputefile :: Int -> IO Int
+mycomputefile v0 = do
+    file <- openFile "log.txt" WriteMode
+    hPutStrLn file ("add3 " ++ show v0)
+    let v1 = add3 v0
+    hPutStrLn file ("mul2 " ++ show v1)
+    let v2 = mul2 v1
+    hPutStrLn file (show v2)
+    hClose file
+    return v2
+
+mycomputeF :: (String -> IO()) -> Int -> IO Int
+mycomputeF logF v0 = do
+    logF ("add3 " ++ show v0)
+    let v1 = add3 v0
+    logF ("mul2 " ++ show v1)
+    let v2 = mul2 v1
+    return v2
+
+
 main :: IO ()
 main = do
     putStrLn "this is log-hs"
     
     res <- mycompute 18
     print res
+
+    res2 <- mycomputefile 18
+    print res2
+
+    res3 <- mycomputeF putStrLn 18
+    print res3
+
+    hF <- openFile "logF.txt" WriteMode
+    resFileF <- mycomputeF (hPutStrLn hF) 18
+    hClose hF
+    print resFileF

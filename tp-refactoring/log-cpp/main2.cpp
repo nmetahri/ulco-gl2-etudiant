97ab0dc25a77769d40fe0503028dc8104565088e
metahri.nafaa <metahrinafaapro@gmail.com>
2024-03-11 15:42:26 +0100

    TP-Refactoring - Injection de dépendances C++

--- tp-refactoring/log-cpp/main2.cpp
+++ tp-refactoring/log-cpp/main2.cpp
@@ -12,8 +12,28 @@ int mul2(int n) {
     return n*2;
 }
 
+int mycomputefile(logFunc_t logF, int v0) {
+    logF("add3 " + std::to_string(v0));
+    const int v1 = add3(v0);
+    logF("mul2 " + std::to_string(v1));
+    const int v2 = add3(v1);
+
+    return v2;
+}
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
+
+    const auto &res = mycomputefile([](const std::string &text) -> void {
+        std::cout << text << std::endl; 
+    }, 5);
+    std::cout << res << std::endl;
+
+    std::ofstream file("log2.txt");
+    const auto &res2 = mycomputefile([&file](const std::string &text) -> void {
+        file << text << std::endl;
+    }, 5);
+    std::cout << res2 << std::endl;
 
     return 0;
 }
